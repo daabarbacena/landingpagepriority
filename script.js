f2d01ea6957406f7119ad28751bbd23ea748a606
Daniel Barbacena <danielbtm@outlook.com>
2024-05-14 10:51:27 -0300

    change style

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 $(document).ready(function () {
-    $("header nav h2.sobrenos").click(function () {
+    $(".header nav h2.sobrenos").click(function () {
         const destino = $("section.sobre");
         $("html").animate({
             scrollTop: destino.offset().top
         }, 500)
     })
 
-    $("header nav h2.comofunciona").click(function () {
+    $(".header nav h2.comofunciona").click(function () {
         const destino = $("section.comoFunciona");
         $("html").animate({
             scrollTop: destino.offset().top
         }, 500)
     })
 
-    $("header nav h2.duvidas").click(function () {
+    $(".header nav h2.duvidas").click(function () {
         const destino = $("section.section-duvidas");
         $("html").animate({
             scrollTop: destino.offset().top
         }, 500)
     })
 
-    $("header nav h2.contato").click(function () {
+    $(".header nav h2.contato").click(function () {
         const destino = $("section.section-contato");
         $("html").animate({
             scrollTop: destino.offset().top
         }, 500)
     })
+
+    /* SEÇÃO SOBRE NÓS */
+
+    $("nav ul li.sobrenos").click(function () {
+        const destino = $("section.sobre");
+        $("html").animate({
+            scrollTop: destino.offset().top
+        }, 500)
+    })
+
+    $("nav ul li.comofunciona").click(function () {
+        const destino = $("section.comoFunciona");
+        $("html").animate({
+            scrollTop: destino.offset().top
+        }, 500)
+    })
+
+    $("nav ul li.duvidas").click(function () {
+        const destino = $("section.section-duvidas");
+        $("html").animate({
+            scrollTop: destino.offset().top
+        }, 500)
+    })
+
+    $("nav ul li.contato").click(function () {
+        const destino = $("section.section-contato");
+        $("html").animate({
+            scrollTop: destino.offset().top
+        }, 500)
+    })
+
+    /* SEÇÃO SOBRE NÓS */
 
     function mostrarResposta(elementoResposta) {
         if (elementoResposta.is(':visible')) {
@@ -46,6 +78,8 @@ $(document).ready(function () {
         });
     });
 
+    /* SEÇÃO SOBRE NÓS */
+
     $("button.estou-interessado").click(function () {
         const destino = $(".form");
         $("html").animate({
@@ -53,13 +87,20 @@ $(document).ready(function () {
         }, 500)
     })
 
+    /* SEÇÃO SOBRE NÓS */
+
+    $("#campo-cpf").mask("000-000-000.00");
+    $("#campo-telefone").mask("(00) 00000-0000");
+
+    /* SEÇÃO SOBRE NÓS */
+
+    $(".menu-hamburguer").click(function(){
+        $(".container-menu").slideToggle();
+    })
+
 });
 
-$(document).ready(function () {
-    $('button.submit').click(function(){
-        alert("Cadastrado!");
-    });
-});
+
 
 
 
